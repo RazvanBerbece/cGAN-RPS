eda7b8d2dd4cf3fc0d4b4937a7d150278294f47f
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2021-09-30 01:35:44 +0100

    IMPLEMENT Generator. Discriminator to be implemented. Other small refactors. README update.

--- main.py
+++ main.py
@@ -5,7 +5,38 @@ import os
 # from model.classes.prep.Dataset import Dataset
 from model.classes.networks.generator.Generator import Generator
 
+# Data Prepping
 # dataset = Dataset(dataset='RockPaperScissors', seed=512, trainRatio='75%', batchSize=128)
 
-generator = Generator(num_classes=3)
-generator.activate_label()
+# CONSTANTS
+num_classes = 3
+
+# HYPERPARAMETERS
+# Generator
+embedding_size              = 100
+label_num_nodes             = '4x4'
+noise_num_nodes             = 512
+generator_initial_num_nodes = 64
+# Discriminator
+# TODO
+# Training
+# TODO
+
+# Generator Init & Config
+generator = Generator()
+generator_model = generator.get_generator_network(           \
+    num_classes=num_classes,                                 \
+    embedding_size=embedding_size,                           \
+    label_num_nodes=label_num_nodes,                         \
+    noise_num_nodes=noise_num_nodes,                         \
+    generator_initial_num_nodes=generator_initial_num_nodes)
+print(generator_model)
+
+# Discriminator Init & Config
+# TODO
+
+# Training
+# TODO
+
+# Generate Image Using Trained cGAN model
+# TODO
--- model/classes/networks/generator/generator.py
+++ model/classes/networks/generator/generator.py
@@ -28,22 +28,107 @@ class Generator:
         <Params>
         num_classes      = number of classes (eg: 3 [rock, paper, scissors])
         embedding_size   = size of embedding (see NLP common practices)
-        num_nodes        = number of nodes for the fully connected Dense layer
+        num_nodes        = number of nodes for the fully connected Dense layer (ie: '3x3', '4x4', '4x5')
     """
     def activate_label_input(self, num_classes, embedding_size, num_nodes):
+        # Get node structure from argument
+        # TODO: Find out why there is a dependency on the number of nodes for the reshaped layer
+        #       1. For merging of the noise matrix output after activations & label output after activations
+        sizes = num_nodes.split("x")
+        self.embedding_row_size = int(sizes[0])
+        self.embedding_col_size = int(sizes[1])
+        # Network Layers
         output_label_embedded   = layers.Embedding(num_classes, embedding_size)(self.input_label) # embed the categorical input
-        output_label_dense      = layers.Dense(num_nodes)(output_label_embedded) # basic matrix multiplication of form output = activation(dot(input, kernel) + bias)
-        output_label_reshape    = layers.Reshape((4, 4, 1))(output_label_dense) 
+        output_label_dense      = layers.Dense(self.embedding_row_size * self.embedding_col_size)(output_label_embedded) # basic matrix multiplication of form output = activation(dot(input, kernel) + bias)
+        output_label_reshape    = layers.Reshape((self.embedding_row_size, self.embedding_col_size, 1))(output_label_dense)
         return output_label_reshape
 
     """
         Gets output for the noise input nodes
 
         <Params>
-        num_nodes  = number of nodes for the fully connected Dense layer 
+        num_nodes  = number of nodes for the fully connected Dense layer
     """
     def activate_noise_input(self, num_nodes):
+        # Network Layers
         output_noise_dense      = layers.Dense(num_nodes)(self.input_noise)
         output_noise_relu       = layers.ReLU()(output_noise_dense)
-        output_noise_reshape    = layers.Reshape()(output_noise_relu)
+        output_noise_reshape    = layers.Reshape((self.embedding_row_size, self.embedding_col_size, num_nodes))(output_noise_relu)
         return output_noise_reshape
+
+    """
+        Gets a Generator ML model
+        Defines the layer architecture using the merged outputs of the label & noise matrixes inputs
+        Currently supports 4 hidden layers
+        TODO: Dynamic number of layers
+
+        <Params>
+        num_classes                 = number of classes (eg: 3 [rock, paper, scissors])
+        embedding_size              = size of embedding (see NLP common practices)
+        label_num_nodes             = number of nodes for the fully connected Dense layer (ie: '3x3', '4x4', '4x5')
+        noise_num_nodes             = number of nodes for the fully connected Dense layer
+        generator_initial_num_nodes = initial number of nodes for the CNN layers (from argument value decrements as num x K, num x (K / 2), ..., num x 1)
+    """
+    def get_generator_network(self, num_classes, embedding_size, label_num_nodes, noise_num_nodes, generator_initial_num_nodes):
+        # Merge label + noise output matrices
+        initial_label_output    = self.activate_label_input(num_classes, embedding_size, label_num_nodes)
+        initial_noise_output    = self.activate_noise_input(noise_num_nodes) # HAS to run AFTER activate_label_input()
+        merged_net_input        = layers.Concatenate()([initial_label_output, initial_noise_output])
+        # Generator CNN Architecture + Forwarding
+        # x is the input that is fordward fed through the network
+        x = layers.Conv2DTranspose(           \
+             generator_initial_num_nodes * 8, \
+             kernel_size=4,                   \
+             strides=2,                       \
+             padding='same',                  \
+             kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.02), \
+             use_bias=False,                  \
+             name='conv_transpose_1')(merged_net_input)
+        x = layers.BatchNormalization(momentum=0.1, epsilon=0.8, center=1.0, scale=0.02, name='bn_1')(x)
+        x = layers.ReLU(name='relu_1')(x)
+
+        x = layers.Conv2DTranspose(           \
+             generator_initial_num_nodes * 4, \
+             kernel_size=4,                   \
+             strides=2,                       \
+             padding='same',                  \
+             kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.02), \
+             use_bias=False,                  \
+             name='conv_transpose_2')(x)
+        x = layers.BatchNormalization(momentum=0.1, epsilon=0.8, center=1.0, scale=0.02, name='bn_2')(x)
+        x = layers.ReLU(name='relu_2')(x)
+
+        x = layers.Conv2DTranspose(           \
+             generator_initial_num_nodes * 2, \
+             kernel_size=4,                   \
+             strides=2,                       \
+             padding='same',                  \
+             kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.02), \
+             use_bias=False,                  \
+             name='conv_transpose_3')(x)
+        x = layers.BatchNormalization(momentum=0.1, epsilon=0.8,  center=1.0, scale=0.02, name='bn_3')(x)
+        x = layers.ReLU(name='relu_3')(x)
+
+        x = layers.Conv2DTranspose(       \
+             generator_initial_num_nodes, \
+             kernel_size=4,               \
+             strides=2,                   \
+             padding='same',              \
+             kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.02), \
+             use_bias=False,              \
+             name='conv_transpose_4')(x)
+        x = layers.BatchNormalization(momentum=0.1, epsilon=0.8, center=1.0, scale=0.02, name='bn_4')(x)
+        x = layers.ReLU(name='relu_4')(x)
+        # Output
+        output = layers.Conv2DTranspose(\
+            3,                          \
+            kernel_size=4,              \
+            strides=2,                  \
+            padding='same',             \
+            kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.02), \
+            use_bias=False,             \
+            activation='tanh',          \
+            name='conv_transpose_6')(x)
+        # Generator Model
+        model = tf.keras.Model([self.input_label, self.input_noise], output)
+        return model
